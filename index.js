2e61df713928859bfd044e254220984034cf15b8
Tobias Jung <jung-tobias@outlook.com>
2020-05-12 01:56:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
+var udp = require("dgram");
+var server = udp.createSocket("udp4");
 
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+// emits when any error occurs
+server.on('error',function(error){
+  console.log('Error: ' + error);
+  server.close();
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+// emits on new datagram msg
+server.on('message',function(msg,info){
+
+console.log(msg);
+  console.log(info);
 });
 
+//emits when socket is ready and listening for datagram msgs
+server.on('listening',function(){
+  var address = server.address();
+  var port = address.port;
+  var family = address.family;
+  var ipaddr = address.address;
+  console.log('Server is listening at port' + port);
+  console.log('Server ip :' + ipaddr);
+  console.log('Server is IP4/IP6 : ' + family);
+});
+
+//emits after the socket is closed using socket.close();
+server.on('close',function(){
+  console.log('Socket is closed !');
+});
+
+server.bind(53); 
